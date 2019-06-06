4fb39c31cce70d78a8f3f63d3d9acf9d25ee7576
Alexander Morozov <frostmoroz83@yandex.ru>
2019-06-06 08:53:33 +0400

    delete branch

--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ToggleMenu from "./Toggle-menu";
 
 const Navigation = () => {
 	return (
 		<nav className="navigation">
+			{/*<ToggleMenu/>*/}
 			<ul className="navigation__list">
 				<li><a href="/">Резюме</a></li>
 				<li><a href="/">Портфолио</a></li>
